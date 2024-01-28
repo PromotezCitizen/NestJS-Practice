fdf3e1800136ef1d7201b7c30ff6958bb33ed7b7
PromotezCitizen <gkstkdgus821@gmail.com>
2024-01-28 23:22:31 +0900

    static 경로를 사용하기 위한 app.use 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Axios Error 해결
+  app.use(express.static(join(__dirname, '..', 'static', 'for_test_html')));
   await app.listen(3000);
 }
 bootstrap();
