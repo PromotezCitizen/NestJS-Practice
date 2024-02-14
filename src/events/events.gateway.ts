5bc1788ff0322d151fe2c6d6bb3cc5810e82f5d3
PromotezCitizen <gkstkdgus821@gmail.com>
2024-02-14 22:12:27 +0900

    웹소켓 구현 + 헤더로 토큰 전송 구현

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,6 +1,6 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
-import { Observable, from, map } from 'rxjs';
-import { Server } from 'socket.io';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Observable } from 'rxjs';
+import { Server, Socket } from 'socket.io';
 
 // @WebSocketGateway(81, {transports: ['websocket']}) // 81번 포트를 'websocket'으로 쓰겠다 선언
 @WebSocketGateway({
@@ -8,17 +8,39 @@ import { Server } from 'socket.io';
     origin: '*',
   },
 }) // 기본 포트로 설정(3000)
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  // socket.io 이용하기 위한 방법
   @WebSocketServer()
   server: Server;
-  
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> { 
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'message', data: item })));
+
+  private clients: Set<Socket> = new Set();
+
+  handleConnection(client: Socket, ...args: any[]) {
+    const headers = client.handshake.headers;
+    if (headers.authorization !== "Bearer asdf.zxcv.qwer") {
+      client.send("disconnected");
+      client.disconnect();
+      return;
+    }
+    client.send("conneted");
+    this.clients.add(client);
   }
 
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+  handleDisconnect(client: Socket) {
+    this.clients.delete(client);
+  }
+  
+  @SubscribeMessage('events')
+  findAll(
+    @MessageBody() data: any, 
+    // 클라이언트 정보를 확인하기 위해서는 ConnectedSocket 어노테이션 사용
+    @ConnectedSocket() sender: Socket
+  ): Observable<WsResponse<number>> {
+    this.clients.forEach( (client) => {
+      if (client !== sender) {
+        client.send(data);
+      }
+    });
+    return;
   }
 }
